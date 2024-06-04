3ee62ae079143db587361829063972aa2678ee32
RMRAmadushanka <rmramadushanka@gmail.com>
2024-06-04 23:54:27 +0530

    Added sub category model

---
+++ backend/repository/model/subCategory.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const subCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    status: {
+      type: String,
+      enum: ['Active', 'Inactive'],
+      default: 'Active',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const SubCategory = mongoose.model('SubCategory', subCategorySchema);
+export default SubCategory;
