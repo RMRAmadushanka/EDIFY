185a99f98ad687fd73aa7a059a4c1d520c263997
RMRAmadushanka <rmramadushanka@gmail.com>
2024-06-17 10:00:57 +0530

    Added Category services

---
+++ backend/services/category.service.js
@@ -0,0 +1,17 @@
+import { getRepositoryFactor } from '../utils/application-store.js';
+import REPOSITORY_TYPES from '../repository/config/repository-types.js';
+import * as s3Service from './s3.service.js';
+
+export const addCategory = async (categoryData) => {
+  const categoryRepository = getRepositoryFactor().get(REPOSITORY_TYPES.CATEGORY);
+  return categoryRepository.save(categoryData);
+};
+
+export const getCategories = async (filter, options) => {
+  const categoryRepository = getRepositoryFactor().get(REPOSITORY_TYPES.CATEGORY);
+  return categoryRepository.getPaginatedList(filter, options);
+};
+
+export const uploadCategoryLogo = async (files) => {
+  return s3Service.uploadFiles(files, 'category_logos');
+};
