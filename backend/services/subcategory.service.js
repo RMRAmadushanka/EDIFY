3f6e7efe52252a2843f48fb4a76eba44fd4362fa
RMRAmadushanka <rmramadushanka@gmail.com>
2024-06-25 21:45:54 +0530

    code clean

--- backend/services/subcategory.service.js
+++ backend/services/subcategory.service.js
@@ -1,14 +1,18 @@
-import { getRepositoryFactor } from '../utils/application-store.js';
-import REPOSITORY_TYPES from '../repository/config/repository-types.js';
+import { getRepositoryFactor } from "../utils/application-store.js";
+import REPOSITORY_TYPES from "../repository/config/repository-types.js";
 
 export const addSubCategory = async (subCategoryData) => {
-  const subCategoryRepository = getRepositoryFactor().get(REPOSITORY_TYPES.SUBCATEGORY);
+  const subCategoryRepository = getRepositoryFactor().get(
+    REPOSITORY_TYPES.SUBCATEGORY
+  );
   return subCategoryRepository.save(subCategoryData);
 };
 
 export const getSubCategories = async (filter, options) => {
   const { limit, page, sortBy } = options;
-  const subCategoryRepository = getRepositoryFactor().get(REPOSITORY_TYPES.SUBCATEGORY);
+  const subCategoryRepository = getRepositoryFactor().get(
+    REPOSITORY_TYPES.SUBCATEGORY
+  );
   const queryOptions = {
     limit,
     page,
@@ -16,12 +20,15 @@ export const getSubCategories = async (filter, options) => {
   };
 
   if (sortBy) {
-    const sortByArray = sortBy.split(':');
-    queryOptions.sort[sortByArray[0]] = sortByArray[1] === 'desc' ? -1 : 1;
+    const sortByArray = sortBy.split(":");
+    queryOptions.sort[sortByArray[0]] = sortByArray[1] === "desc" ? -1 : 1;
   }
 
   if (filter.search) {
-    return subCategoryRepository.getPaginatedListWithSearch(filter, queryOptions);
+    return subCategoryRepository.getPaginatedListWithSearch(
+      filter,
+      queryOptions
+    );
   } else {
     return subCategoryRepository.getPaginatedList(filter, queryOptions);
   }
