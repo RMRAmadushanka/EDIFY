b6a22be093cd1f2bdda105a8cb3d3017d518e0a6
RMRAmadushanka <rmramadushanka@gmail.com>
2024-07-28 22:59:59 +0530

    fixed

--- frontend/src/features/category/constants.js
+++ frontend/src/features/category/constants.js
@@ -1,13 +1,12 @@
-
 const API = {
-    POST_CREATE_CATEGORY: {
-        path:'/category/',
-        method: 'POST'
-    },
-    POST_CATEGORY_LOGO:{
-        path:'/category/upload-category-logo',
-        method:'POST'
-    }
-}
+  POST_CREATE_CATEGORY: {
+    path: "/category/",
+    method: "POST",
+  },
+  POST_CATEGORY_LOGO: {
+    path: "/category/upload-category-logo",
+    method: "POST",
+  },
+};
 
 export default API;
