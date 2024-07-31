920d3d6f37c4bb73026303fe27422f59659348a3
RMRAmadushanka <rmramadushanka@gmail.com>
2024-07-31 21:25:44 +0530

    update slice for sub category

--- frontend/src/features/category/slice.js
+++ frontend/src/features/category/slice.js
@@ -7,14 +7,17 @@ export const initialState = {
   subCategories: [],
   uploadLogo: {},
   addCategoryLoading: false,
+  addSubCategoryLoading: false,
   errorCode: "",
   message: "",
-  pagination: {
-    total: 0,
-    limit: 10,
-    page: 1,
-    totalPages: 0,
-  },
+  pagination: [
+    {
+      total: 0,
+      limit: 10,
+      page: 1,
+      totalPages: 0,
+    },
+  ],
 };
 
 export const categorySlice = createSlice({
@@ -26,10 +29,27 @@ export const categorySlice = createSlice({
     },
     addCategorySucceeded(state, action) {
       state.addCategoryLoading = false;
-      state.categories = action.payload;
+      state.categories = [...state.categories, action.payload];
     },
     addCategoryFailed(state, action) {
       state.addCategoryLoading = false;
+      state.errorCode = action.payload.code;
+      state.message = action.payload.message;
+    },
+    addSubCategory(state) {
+      state.addSubCategoryLoading = true;
+    },
+    addSubCategorySucceeded(state, action) {
+      state.addSubCategoryLoading = false;
+      state.subCategories = [...state.subCategories, action.payload];
+      // Directly add the new subcategory to the corresponding category
+      const category = state.categories.find(
+        (category) => category._id === action.payload.category
+      );
+      category?.subcategories.push(action.payload);
+    },
+    addSubCategoryFailed(state, action) {
+      state.addSubCategoryLoading = false;
       state.errorCode = action.payload.code;
       state.message = action.payload.message;
     },
@@ -45,6 +65,25 @@ export const categorySlice = createSlice({
     uploadCategoryLogoFailed(state) {
       state.logoUploadLoading = false;
       state.logoUploadCompleted = true;
+    },
+    fetchCategories(state) {
+      state.addCategoryLoading = true;
+    },
+    fetchCategoriesSucceeded(state, action) {
+      state.addCategoryLoading = false;
+      state.categories = action.payload.data;
+      state.pagination = {
+        ...state.pagination,
+        total: action.payload.total,
+        limit: action.payload.limit,
+        page: action.payload.page,
+        totalPages: action.payload.totalPages,
+      };
+    },
+    fetchCategoriesFailed(state, action) {
+      state.addCategoryLoading = false;
+      state.errorCode = action.payload.code;
+      state.message = action.payload.message;
     },
   },
 });
